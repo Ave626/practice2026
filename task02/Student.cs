011582adb154369e78165746b707b084c5f421fa
AveMint <itped7@gmail.com>
2026-06-25 04:07:43 +0600

    fix: resolve nullable warnings in Student class

--- task02/Student.cs
+++ task02/Student.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace task02;
 public class Student
 {
-    public string Name { get; set; }
-    public string Faculty { get; set; }
-    public List<int> Grades { get; set; }
+    public required string Name { get; set; }
+    public required string Faculty { get; set; }
+    public required List<int> Grades { get; set; }
 }
