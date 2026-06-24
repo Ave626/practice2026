d6619c541875b9d23b8602ecb31a3e6a99aeb5d8
AveMint <itped7@gmail.com>
2026-06-25 04:00:46 +0600

    feat: implement StudentService and xUnit tests

---
+++ task02/Student.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace task02;
+public class Student
+{
+    public string Name { get; set; }
+    public string Faculty { get; set; }
+    public List<int> Grades { get; set; }
+}
