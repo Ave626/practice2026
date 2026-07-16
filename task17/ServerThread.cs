96ef1958131c977434499054bcd3ffb32412dede
Ave626 <itped7@gmail.com>
2026-07-16 18:27:17 +0600

    feat: integrate scheduler into ServerThread default strategy and soft stop

--- task17/ServerThread.cs
+++ task17/ServerThread.cs
@@ -7,21 +7,25 @@ namespace task17;
 public class ServerThread
 {
     private readonly BlockingCollection<ICommand> _queue = new();
-    
+    private readonly IScheduler? _scheduler;
     private Action _strategy;
-    
     private readonly Thread _thread;
-    
     private bool _stopRequested = false;
 
-    public ServerThread()
+    public ServerThread() : this(null)
     {
+    }
+
+    public ServerThread(IScheduler? scheduler)
+    {
+        _scheduler = scheduler;
         _strategy = DefaultStrategy;
         _thread = new Thread(RunLoop);
     }
 
     public Thread Thread => _thread;
     public BlockingCollection<ICommand> Queue => _queue;
+    public IScheduler? Scheduler => _scheduler;
 
     public void Start() => _thread.Start();
 
@@ -47,20 +51,34 @@ public class ServerThread
 
         UpdateStrategy(() =>
         {
-            if (_queue.TryTake(out var command))
+            ICommand? command = null;
+            
+            if (_queue.TryTake(out var cmd))
             {
-                try
-                {
-                    command.Execute();
-                }
-                catch (Exception ex)
-                {
-                    ExceptionHandler.Handle?.Invoke(ex, command);
-                }
+                command = cmd;
+            }
+            else if (_scheduler != null && _scheduler.HasCommand())
+            {
+                command = _scheduler.Select();
             }
             else
             {
                 _stopRequested = true;
+                return;
+            }
+
+            try
+            {
+                command.Execute();
+
+                if (command is ILongCommand longCommand && !longCommand.IsCompleted)
+                {
+                    _scheduler.Add(command);
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.Handle?.Invoke(ex, command);
             }
         });
     }
@@ -78,8 +96,34 @@ public class ServerThread
         ICommand? command = null;
         try
         {
-            command = _queue.Take();
-            command.Execute();
+            if (_scheduler == null)
+            {
+                command = _queue.Take();
+                command.Execute();
+            }
+            else
+            {
+                int timeout = _scheduler.HasCommand() ? 0 : Timeout.Infinite;
+
+                if (_queue.TryTake(out var cmd, timeout))
+                {
+                    command = cmd;
+                }
+                else if (_scheduler.HasCommand())
+                {
+                    command = _scheduler.Select();
+                }
+
+                if (command != null)
+                {
+                    command.Execute();
+
+                    if (command is ILongCommand longCommand && !longCommand.IsCompleted)
+                    {
+                        _scheduler.Add(command);
+                    }
+                }
+            }
         }
         catch (InvalidOperationException)
         {
